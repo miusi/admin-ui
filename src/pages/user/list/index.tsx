7a84798c8cad607d7275309437547ef4df1e4b08
Godric <miaochengod@outlook.com>
2020-03-19 23:45:19 +0800

    build user search list

---
+++ src/pages/user/list/index.tsx
@@ -0,0 +1,12 @@
+import { Radio, Input } from "antd";
+
+/*
+ * @Description: 
+ * @Author: godric
+ * @Date: 2020-03-19 23:35:00
+ * @LastEditTime: 2020-03-19 23:44:40
+ * @LastEditors: godric
+ */
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const { Search } = Input;
