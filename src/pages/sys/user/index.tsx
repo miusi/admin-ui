5acd83ef4f330d1c25dd22c971d15c0a5725b2f6
Godric <miaochengod@outlook.com>
2020-03-28 15:13:10 +0800

    Update index.tsx

--- src/pages/sys/user/index.tsx
+++ src/pages/sys/user/index.tsx
@@ -1,22 +1,20 @@
 /*
-* @Description: 
-* @Author: godric
-* @Date: 2020-03-19 23:35:00
- * @LastEditTime: 2020-03-23 22:20:20
+ * @Description:
+ * @Author: godric
+ * @Date: 2020-03-19 23:35:00
+ * @LastEditTime: 2020-03-28 15:13:06
  * @LastEditors: godric
-*/
+ */
 
-import { useState, useRef } from "react";
-import ProTable, { ProColumns, ActionType } from "@ant-design/pro-table";
-import { TableListItem } from "./data.d";
+import React, { useState, useRef } from 'react';
+import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { message, Button } from "antd";
-import { FormValueType } from "@/pages/user/list/components/UpdateForm";
-import { add, queryList, remove, update } from "./service";
-import CreateForm from "./components/CreateFrom";
-import React from "react";
-import { PlusOutlined } from "@ant-design/icons";
-
+import { message, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { TableListItem } from './data.d';
+import { FormValueType } from './components/UpdateForm';
+import { add, queryList } from './service';
+import CreateForm from './components/CreateFrom';
 
 /**
  * 添加用户
@@ -27,10 +25,10 @@ const handleAdd = async (fields: FormValueType) => {
   try {
     await add({
       id: fields.id,
-      userName: fields.userName,
+      userName: fields.username,
       name: fields.name,
       phone: fields.phone,
-      password: fields.password
+      password: fields.password,
     });
     hide();
     message.success('添加成功');
@@ -42,58 +40,14 @@ const handleAdd = async (fields: FormValueType) => {
   }
 };
 
-/**
- * 更新
- * @param fields
- */
-const handleUpdate = async (fields: FormValueType) => {
-  const hide = message.loading('正在跟新');
-  try {
-    await update({
-      id: fields.id,
-      userName: fields.userName,
-      name: fields.name,
-      phone: fields.phone
-    });
-    hide();
-
-    message.success('配置成功');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('配置失败请重试！');
-    return false;
-  }
-};
-
-
-/**
- *  删除节点
- * @param selectedRows
- */
-const handleRemove = async (selectedRows: TableListItem[]) => {
-  const hide = message.loading('正在删除');
-  if (!selectedRows) return true;
-  try {
-    await remove({
-      key: selectedRows.map(row => row.id),
-    });
-    hide();
-    message.success('删除成功，即将刷新');
-    return true;
-  } catch (error) {
-    hide();
-    message.error('删除失败，请重试');
-    return false;
-  }
-};
 const UserList: React.FC<{}> = () => {
   const [createModalVisible, handleModalVisible] = useState<boolean>(false);
   const actionRef = useRef<ActionType>();
   const columns: ProColumns<TableListItem>[] = [
     {
       title: '登录名',
-      dataIndex: 'userName',
+      dataIndex: 'username',
+      hideInSearch: true,
     },
     {
       title: '姓名',
@@ -108,50 +62,53 @@ const UserList: React.FC<{}> = () => {
       dataIndex: 'status',
       valueEnum: {
         0: { text: '无效', status: 'Default' },
-        1: { text: '有效', status: 'Processing' }
+        1: { text: '有效', status: 'Processing' },
       },
+      hideInSearch: true,
     },
     {
       title: '创建人',
-      dataIndex: 'createBy'
+      dataIndex: 'createBy',
+      hideInSearch: true,
     },
     {
       title: '创建时间',
       dataIndex: 'createDate',
       sorter: true,
       valueType: 'dateTime',
+      hideInSearch: true,
     },
     {
       title: '更新人',
-      dataIndex: 'updateBy'
+      dataIndex: 'updateBy',
+      hideInSearch: true,
     },
     {
       title: '更新时间',
       dataIndex: 'updateDate',
       sorter: true,
       valueType: 'dateTime',
+      hideInSearch: true,
     },
     {
       title: '操作',
       dataIndex: 'option',
       valueType: 'option',
-
     },
   ];
 
   return (
-    //根据用户权限判断显示按钮
+    // 根据用户权限判断显示按钮
     <PageHeaderWrapper>
       <ProTable<TableListItem>
         headerTitle="用户列表"
         actionRef={actionRef}
-        rowKey="key"
-        toolBarRender={(action, { selectedRows }) => [
-          //TODO 根据权限判断显示的按钮
+        rowKey="id"
+        toolBarRender={() => [
+          // TODO 根据权限判断显示的按钮
           <Button icon={<PlusOutlined />} type="primary" onClick={() => handleModalVisible(true)}>
             新建
-                </Button>
-
+          </Button>,
         ]}
         request={params => queryList(params)}
         columns={columns}
